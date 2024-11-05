f876d3ec632c94784eef475a1894be1f6d6ef106
1760241 <luuthienvi.github@gmail.com>
2024-11-05 12:50:51 +0700

    feat: add menu

--- menu/menu.c
+++ menu/menu.c
@@ -1,40 +1,23 @@
 #include "menu.h"
 #include "../patron/patron.h"
+#include "../utils/format.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
-void Menu()
-{
-    MenuInput();
-}
-
-static void MenuDetails()
-{
-    puts("\n-------------------------------------------------------------------------------------------");
-    puts("--------------------------- HCMUS Library Manager ---------------------------");
-    puts("\t(1) Show patrons");
-    puts("\t(2) Add a patron");
-    puts("\t(0) Exit");
-}
-
-static void MenuInput()
+void input_menu()
 {
     int opt = 0;
-    int shouldExit = 0;
-
+    int should_exit = 0;
     do
     {
-        MenuDetails();
+        main_menu();
         printf("\nSelect an option: ");
+        if (scanf("%d", &opt) != 1 || opt < 0 || opt > 5)
+        {
 
-        if (scanf("%d", &opt) != 1)
-        {
-            puts("\n(!) Invalid option. Please re-enter a number between 0 and 2.");
+            puts("\n(!) Invalid option. Please re-enter a number between 0 and 5.");
             _flushall();
-            continue;
-        }
-        if (opt < 0 || opt > 2)
-        {
-            puts("\n(!) Invalid option. Please re-enter a number between 0 and 2.");
             continue;
         }
 
@@ -42,25 +25,86 @@ static void MenuInput()
         {
         case 1:
         {
-            InputPatrons();
-            puts("\n(?) Continue?");
+            input_patron_menu();
+            should_exit = 1;
             break;
         }
-
-        case 2:
-        {
-            OutputPatrons();
-            puts("\n(?) Continue?");
-            break;
-        }
-
         case 0:
         {
             puts("\nSee you next time <3");
-            shouldExit = 1;
+            should_exit = 1;
             break;
         }
-        };
+        }
+    } while (!should_exit);
+}
 
-    } while (!shouldExit);
+static void main_menu()
+{
+    system("cls");
+    puts(HORIZONTAL_LINE);
+    puts("\t1. Patron Management");
+    puts("\t2. Book Management");
+    puts("\t3. Book Loan");
+    puts("\t4. Book Return");
+    puts("\t5. Statistics");
+    puts("\t0. Exit");
+    puts(HORIZONTAL_LINE);
+}
+
+static void patron_menu()
+{
+    system("cls");
+    puts(HORIZONTAL_LINE);
+    puts("\t1. View Patrons");
+    puts("\t2. Add New Patron");
+    puts("\t3. Update Patron");
+    puts("\t4. Delete Patron");
+    puts("\t5. Find Patron by Citizen ID");
+    puts("\t6. Find Books Borrowed by Full Name");
+    puts("\t0. Return");
+    puts(HORIZONTAL_LINE);
+}
+
+static void input_patron_menu()
+{
+    int opt = 0;
+    int should_exit = 0;
+
+    do
+    {
+        patron_menu();
+        printf("\nSelect an option: ");
+        if (scanf("%d", &opt) != 1 || opt < 0 || opt > 6)
+        {
+
+            puts("\n(!) Invalid option. Please re-enter a number between 0 and 6.");
+            _flushall();
+            continue;
+        }
+
+        switch (opt)
+        {
+        case 1:
+        {
+            output_patrons();
+            puts("\n\n\n\n\nPress any key to continue...");
+            getch();
+            break;
+        }
+        case 2:
+        {
+            input_patrons();
+            puts("\n\n\n\n\nPress any key to continue...");
+            getch();
+            break;
+        }
+        case 0:
+        {
+            input_menu();
+            should_exit = 1;
+            break;
+        }
+        }
+    } while (!should_exit);
 }
--- menu/menu.h
+++ menu/menu.h
@@ -1,8 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
 
-void Menu();
-static void MenuInput();
-static void MenuDetails();
+void input_menu();
+static void main_menu();
+static void patron_menu();
+static void input_patron_menu();
 
 #endif // MENU_H
