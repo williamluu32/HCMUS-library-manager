9443daa9b0b6a0a9e99ff78c409734aa4b9c3014
williamluu32 <luuthienvi.github@gmail.com>
2024-11-28 22:49:16 +0700

    feat: Define profile struct

---
+++ src/profile/profile.h
@@ -0,0 +1,20 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#include "../../include/constants/profile_const.h"
+#include "../../include/enumerations/sex.h"
+#include "../utils/date/date.h"
+
+struct _Profile
+{
+    char full_name[FULL_NAME_SIZE + 1];
+    char citizen_id[CITIZEN_ID_SIZE + 1];
+    char emails[EMAIL_SIZE + 1];
+    char addresses[ADDRESS_SIZE + 1];
+    Sex sex;
+    Date dob;
+};
+
+typedef struct _Profile Profile;
+
+#endif // PROFILE_H
