af84cb8d0bcffe7a4e78b487321ff41accd11d59
williamluu32 <luuthienvi.github@gmail.com>
2024-11-28 21:48:39 +0700

    feat: Add is_valid_year to check year validity

--- src/utils/date/date.c
+++ src/utils/date/date.c
@@ -1,7 +1,26 @@
 #include "date.h"
 #include "../../../include/constants/system_const.h"
+#include "../../../include/constants/time_const.h"
 
-int is_leap(int year)
+/////////////////////////////////////////////////////////
+/**
+ * Xác định xem năm đã cho có phải hợp lệ hay không
+ *
+ * @param year năm
+ * @return trả về 1 nếu đó là năm hợp lệ, ngược lại trả về 0
+ */
+static int is_valid_year(int year)
+{
+    return year >= MIN_YEAR && year < MAX_YEAR;
+}
+
+/**
+ * Xác định xem năm đã cho có phải năm nhuận không
+ *
+ * @param year năm
+ * @return trả về 1 nếu đó là năm nhuận, ngược lại trả về 0
+ */
+static int is_leap(int year)
 {
     if (year % 4 == 0)
         return TRUE;
@@ -19,12 +38,29 @@ int is_leap(int year)
     }
 }
 
-int get_days_of_month(const Date *d)
+static int get_days_of_month(const Date *d)
 {
-
-    return 0;
+    switch (d->month)
+    {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        return 30;
+    case 2:
+        return is_leap(d->month) ? 29 : 28;
+    }
 }
 
+/////////////////////////////////////////////////////////
 int is_valid_date(const Date *d)
 {
     // valid year
