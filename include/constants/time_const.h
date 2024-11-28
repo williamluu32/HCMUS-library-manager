d9e5328506cd3b9d8a0f21b61be36e0602add45f
williamluu32 <luuthienvi.github@gmail.com>
2024-11-28 22:47:08 +0700

    feat: Define date and time constants

---
+++ include/constants/time_const.h
@@ -0,0 +1,11 @@
+#ifndef TIME_CONST_H
+#define TIME_CONST_H
+
+#define MIN_YEAR 1900
+#define MAX_YEAR 10000
+#define MIN_MONTH 1
+#define MAX_MONTH 12
+#define MIN_DAY 1
+#define MAX_DAY 31
+
+#endif // TIME_CONST_H
