761d205d0352c8d826b9c1268236cbb8471e9c41
williamluu32 <luuthienvi.github@gmail.com>
2024-11-28 23:29:11 +0700

    feat: Add is_empty_string function to check if a string is empty

--- src/utils/string/string.c
+++ src/utils/string/string.c
@@ -12,3 +12,8 @@ void remove_trailing_newline(char *str)
     if (len > 0 && str[len - 1] == '\n')
         str[len - 1] = '\0';
 }
+
+int is_empty_string(const char *str)
+{
+    return str[0] == '\0';
+}
--- src/utils/string/string.h
+++ src/utils/string/string.h
@@ -1,5 +1,5 @@
-#ifndef STR_H
-#define STR_H
+#ifndef STRING_H
+#define STRING_H
 
 /**
  * Xóa ký tự xuống dòng ('\n') ở cuối chuỗi ký tự.
@@ -9,4 +9,12 @@
  */
 void remove_trailing_newline(char *str);
 
-#endif // STR_H
+/**
+ * Xác định xem có phải là chuỗi rỗng không
+ *
+ * @param str chuỗi cần kiểm tra
+ * @return 1 nếu chuỗi rỗng, ngược lại trả về 0
+ */
+int is_empty_string(const char *str);
+
+#endif // STRING_H
