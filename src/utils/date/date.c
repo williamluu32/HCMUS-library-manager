541f66f4a49d62e3a7e4d04b135f79769d97db87
williamluu32 <luuthienvi.github@gmail.com>
2024-11-28 22:41:57 +0700

    feat: Add is_valid_date function to check date validity

--- src/utils/date/date.c
+++ src/utils/date/date.c
@@ -2,12 +2,12 @@
 #include "../../../include/constants/system_const.h"
 #include "../../../include/constants/time_const.h"
 
-/////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 /**
  * Xác định xem năm đã cho có phải hợp lệ hay không
  *
  * @param year năm
- * @return trả về 1 nếu đó là năm hợp lệ, ngược lại trả về 0
+ * @return 1 nếu đó là năm hợp lệ, ngược lại trả về 0
  */
 static int is_valid_year(int year)
 {
@@ -15,10 +15,21 @@ static int is_valid_year(int year)
 }
 
 /**
+ * Xác định xem tháng đã cho có phải hợp lệ hay không
+ *
+ * @param month tháng
+ * @return 1 nếu đó là tháng hợp lệ, ngược lại trả về 0
+ */
+static int is_valid_month(int month)
+{
+    return month >= MIN_MONTH && month <= MAX_MONTH;
+}
+
+/**
  * Xác định xem năm đã cho có phải năm nhuận không
  *
  * @param year năm
- * @return trả về 1 nếu đó là năm nhuận, ngược lại trả về 0
+ * @return 1 nếu đó là năm nhuận, ngược lại trả về 0
  */
 static int is_leap(int year)
 {
@@ -28,9 +39,19 @@ static int is_leap(int year)
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
-static int get_days_of_month(const Date *d)
+/**
+ * Xác định số ngày của tháng-năm đã cho
+ *
+ * @param month tháng
+ * @param year năm
+ * @return số ngày của tháng, hoặc -1 nếu tháng hoặc năm không hợp lệ
+ */
+static int get_days_of_month(int month, int year)
 {
-    switch (d->month)
+    if (!is_valid_month(month) || !is_valid_year(year))
+        return INVALID;
+
+    switch (month)
     {
     case 1:
     case 3:
@@ -46,15 +67,20 @@ static int get_days_of_month(const Date *d)
     case 11:
         return 30;
     case 2:
-        return is_leap(d->month) ? 29 : 28;
+    {
+        return is_leap(year) ? 29 : 28;
+    }
     }
 }
 
 /////////////////////////////////////////////////////////
 int is_valid_date(const Date *d)
 {
-    // valid year
-    // valid month
-    // valid date by dateofmonth
-    return 0;
+    if (!is_valid_month(d->month) || !is_valid_year(d->year))
+        return FALSE;
+
+    if (d->day < MIN_DAY || d->day > get_days_of_month(d->month, d->year))
+        return FALSE;
+
+    return TRUE;
 }
--- src/utils/date/date.h
+++ src/utils/date/date.h
@@ -1,3 +1,10 @@
+/**
+ * Định nghĩa cấu trúc và các hàm xử lý liên quan tới ngày-tháng-năm (date)
+ *
+ * @author William Luu <24810019@student.hcmus.edu.vn>
+ * @date 2024-11-28
+ */
+
 #ifndef DATE_H
 #define DATE_H
 
@@ -10,8 +17,13 @@ struct _Date
 typedef struct _Date Date;
 
 ////////////////////////////////////////////////////////////////////
-int is_leap(int year);
-int get_days_of_month(const Date *d);
+/**
+ * Xác định xem một ngày-tháng-năm có hợp lệ không
+ *
+ * @param day ngày
+ * @param month tháng
+ * @param year năm
+ * @return 1 nếu ngày hợp lệ, ngược lại trả về 0
+ */
 int is_valid_date(const Date *d);
-
 #endif // DATE_H
