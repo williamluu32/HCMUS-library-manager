8c97b7b38979082517e311e1f21e0ba0fa855d70
williamluu32 <luuthienvi.github@gmail.com>
2024-11-30 19:24:36 +0700

    refactor: (User struct) Optimize memory layout with #pragma pack(1)

--- src/user/user.h
+++ src/user/user.h
@@ -6,14 +6,17 @@
 #include "../../include/enumerations/user_type.h"
 #include "../profile/profile.h"
 
+#pragma pack(1)
 struct _User
 {
     char user_name[MAX_USERNAME_SIZE + 1];
     char password[MAX_PASSWORD_SIZE + 1];
+    int id;
     UserStatus status;
     UserType type;
     Profile profile;
 };
+#pragma pack()
 typedef struct _User User;
 
 #endif // USER_H
